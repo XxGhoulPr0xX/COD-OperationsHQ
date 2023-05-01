8c7035c56dea7fd38548dc3a5f685978e53b1f3b
Francisco Javier Alvarado <93677291+XxGhoulPr0xX@users.noreply.github.com>
2023-04-30 18:38:58 -0600

    Update Unidad 2 Practica°4.sql

--- Unidad 2 Practica°4.sql
+++ Unidad 2 Practica°4.sql
@@ -1,5 +1,4 @@
-use VETERINARIA
-/* Practica ∞4:
+/* Practica ¬∞4:
 	Generar una funcion escalar que reciba un curp valida y retorne la entidad de nacimiento:
 	Ejemplo: TOAO860920H'PL'RLC04 retorna puebla
 */ 
@@ -15,12 +14,12 @@ BEGIN
 	Insert into @Estados (id,code,nombre)
 	VALUES	(1,'AS','AGUASCALIENTES'),(2,'BC','BAJA CALIFORNIA'),(3,'BS','BAJA CALIFORNIA SUR'),(4,'CC','CAMPECHE'),
 			(5,'CL','COAHUILA DE ZARAGOZA'),(6,'CM','COLIMA'),(7,'CS','CHIAPAS'),(8,'CH','CHIHUAHUA'),
-			(9,'DF','CIUDAD DE M…XICO'),(10,'DG','DURANGO'),(11,'GT','GUANAJUATO'),(12,'GR','GUERRERO'),
-			(13,'HG','HIDALGO'),(14,'JC','JALISCO'),(15,'MC','M…XICO'),(16,'MN','MICHOAC¡N DE OCAMPO'),
-			(17,'MS','MORELOS'),(18,'NT','NAYARIT'),(19,'NL','NUEVO LE”N'),(20,'OC','OAXACA'),
-			(21,'PL','PUEBLA'),(22,'QT','QUER…TARO'),(23,'QR','QUINTANA ROO'),(24,'SP','SAN LUIS POTOSÕ'),
+			(9,'DF','CIUDAD DE M√âXICO'),(10,'DG','DURANGO'),(11,'GT','GUANAJUATO'),(12,'GR','GUERRERO'),
+			(13,'HG','HIDALGO'),(14,'JC','JALISCO'),(15,'MC','M√âXICO'),(16,'MN','MICHOAC√ÅN DE OCAMPO'),
+			(17,'MS','MORELOS'),(18,'NT','NAYARIT'),(19,'NL','NUEVO LE√ìN'),(20,'OC','OAXACA'),
+			(21,'PL','PUEBLA'),(22,'QT','QUER√âTARO'),(23,'QR','QUINTANA ROO'),(24,'SP','SAN LUIS POTOS√ç'),
 			(25,'SL','SINALOA'),(26,'SR','SONORA'),(27,'TC','TABASCO'),(28,'TS','TAMAULIPAS'),
-			(29,'TL','TLAXCALA'),(30,'VZ','VERACRUZ DE IGNACIO DE LA LLAVE'),(31,'YN','YUCAT¡N'),(32,'ZS','ZACATECAS')
+			(29,'TL','TLAXCALA'),(30,'VZ','VERACRUZ DE IGNACIO DE LA LLAVE'),(31,'YN','YUCAT√ÅN'),(32,'ZS','ZACATECAS')
 	While @i<= (select COUNT(*) FROM @Estados)
 	BEGIN
 	    IF @codigoEntidad = (select code from @Estados where id=@i)

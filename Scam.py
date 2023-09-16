adb82b8e44ffee5229b17b52df8fef5209942259
Francisco Javier Alvarado <93677291+XxGhoulPr0xX@users.noreply.github.com>
2023-09-15 20:04:54 -0600

    Delete Scam.py

--- Scam.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-whom = "5512762662"
-message = "lol"
-params = {'phone': whom, 'message': message, 'key': 'textbelt'}
-resp = requests.post("https://textbelt.com/text", data=params)
-print(resp.json())
